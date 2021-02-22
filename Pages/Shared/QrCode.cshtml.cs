c275ad5fbd26f82ed5cdb25401d902c43a7eec2f
Daniel Silva <37184876+danielsilvarj@users.noreply.github.com>
2021-02-22 10:05:45 -0300

    Projeto

---
+++ Pages/Shared/QrCode.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QrWeb.Pages
+{
+    public class QrCodeModel : PageModel
+    {
+        public IActionResult OnGet([FromQuery]string url)
+        {
+            var image = QrCodeGenerator.GenerateByteArray(url);
+            return File(image, "image/jpeg");
+
+        }
+    }
+}
